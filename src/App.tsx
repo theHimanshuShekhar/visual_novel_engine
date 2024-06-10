16a0431f2078f116cc1d52b7d19b9e4a240298ac
theHimanshuShekhar <hshekhar.dev@gmail.com>
2024-06-10 13:49:06 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { MainMenu } from "./views/scenes/mainMenu";
+import { SplashScreen } from "./views/scenes/splashScreen";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<SplashScreen />} />
+      <Route path="/main-menu" element={<MainMenu />} />
+    </Routes>
+  );
+}
---
+++ src/views/scenes/mainMenu.tsx
@@ -0,0 +1,3 @@
+export function MainMenu() {
+  return <div>Main Menu</div>;
+}
